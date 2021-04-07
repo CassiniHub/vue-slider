7d92f1ecad26469002dc0b72801363863d97f9ce
Marco Orazio Re <marcooraziore@gmail.com>
2021-04-07 16:54:38 +0200

    Goal
    - Added a functioning slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,44 @@ function init() {
         el: '#app',
 
         data: {
-            log: 'Hello Vue!'
+            images: [
+                'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+                'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+                'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+                'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+            ],
+
+            indexValue: 0,
+            maxIndexValue: 3,
+        },
+
+        methods: {
+            sliderLng: function () {
+                
+                return this.images.length;
+            },
+
+            nextImg: function () {
+                
+                if (this.indexValue < this.maxIndexValue) {
+                    
+                    this.indexValue++;
+                } else {
+
+                    this.indexValue = 0;
+                }
+            },
+
+            prevImg: function () {
+                
+                if (this.indexValue > 0) {
+                    
+                    this.indexValue--;
+                } else {
+
+                    this.indexValue = this.maxIndexValue;
+                }
+            }
         }
     });
 }
